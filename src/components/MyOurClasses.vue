96e67773ec5ef22f7399d009c30785d087ad2d01
Edoardo Lai <edoardolai96@gmail.com>
2022-03-03 18:25:50 +0100

    layout our classes

--- src/components/MyOurClasses.vue
+++ src/components/MyOurClasses.vue
@@ -1,7 +1,181 @@
 <template>
-  <div>
-      
-  </div>
+  <section id="ourclasses" class="debug-container container">
+      <div class="row debug-row">
+        <div class="col debug-col text-center">
+        <h2>Welcome to Fable</h2>
+            <p>Family daycare, preschool, and kindergarten</p>
+            <p>/\/\/\/</p>
+        </div>
+      </div>
+
+      <div class="row debug-row">
+        <div class="col debug-col">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum voluptas, odio debitis sit inventore voluptate delectus nobis, odit, pariatur quisquam sint quo! Et eius numquam tempore eligendi! Voluptas, explicabo? Rem.</p>
+        </div>
+
+        <div class="col debug-col">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed molestiae, quod saepe tempora odit placeat voluptatem corporis accusantium cum velit dolores veritatis, est debitis in magnam beatae! Quod, similique amet.
+          </p>
+        </div>
+      </div>
+
+      <div class="row debug-row gx-4 gy-4">
+
+         <div class="col-6">
+           <div class="ms_card debug-card d-flex">
+             <div class="text-box h-100 w-50 d-flex flex-column bg-primary justify-content-between">
+               <div class="text-box__top">
+               <h2>
+                 Little Lambs
+               </h2>
+               <p>Class Name</p>
+               </div>
+                <div class="text-box_bottom  bg-warning ">
+                  <div class="text-box_bottom_left my-1 w-50 d-inline-block text-center border-end">
+                    <h4 class="my-0">
+                      12-24
+                    </h4>
+                    <p class="my-0">
+                      Years old
+                    </p>
+                  </div>
+                  <div class="text-box_bottom_right my-1 w-50 d-inline-block text-center">
+                    <h4 class="my-0">
+                      9
+                    </h4>
+                    <p class="my-0">
+                      Class Size
+                    </p>
+                  </div>
+                </div>
+
+             </div>
+             <div class="img-box h-100 w-50 bg-secondary position-relative">
+               <img class="w-100 h-100 ms_fit_cover" src="../assets/images/class_01-690x506.jpg" alt="">
+               <button class="ms_btn_primary position-absolute bottom-0 end-0">
+                 Read More >
+               </button>
+             </div>
+           </div>
+         </div>
+
+         <div class="col-6">
+                      <div class="ms_card debug-card d-flex">
+             <div class="text-box h-100 w-50 d-flex flex-column bg-primary justify-content-between">
+               <div class="text-box__top">
+               <h2>
+                 Little Lambs
+               </h2>
+               <p>Class Name</p>
+               </div>
+                <div class="text-box_bottom  bg-warning ">
+                  <div class="text-box_bottom_left my-1 w-50 d-inline-block text-center border-end">
+                    <h4 class="my-0">
+                      12-24
+                    </h4>
+                    <p class="my-0">
+                      Years old
+                    </p>
+                  </div>
+                  <div class="text-box_bottom_right my-1 w-50 d-inline-block text-center">
+                    <h4 class="my-0">
+                      9
+                    </h4>
+                    <p class="my-0">
+                      Class Size
+                    </p>
+                  </div>
+                </div>
+
+             </div>
+             <div class="img-box h-100 w-50 bg-secondary position-relative">
+               <img class="w-100 h-100 ms_fit_cover" src="../assets/images/class_01-690x506.jpg" alt="">
+               <button class="ms_btn_primary position-absolute bottom-0 end-0">
+                 Read More >
+               </button>
+             </div>
+           </div>
+
+         </div>
+         <div class="col-6">
+           <div class="ms_card debug-card d-flex">
+             <div class="text-box h-100 w-50 d-flex flex-column bg-primary justify-content-between">
+               <div class="text-box__top">
+               <h2>
+                 Little Lambs
+               </h2>
+               <p>Class Name</p>
+               </div>
+                <div class="text-box_bottom  bg-warning ">
+                  <div class="text-box_bottom_left my-1 w-50 d-inline-block text-center border-end">
+                    <h4 class="my-0">
+                      12-24
+                    </h4>
+                    <p class="my-0">
+                      Years old
+                    </p>
+                  </div>
+                  <div class="text-box_bottom_right my-1 w-50 d-inline-block text-center">
+                    <h4 class="my-0">
+                      9
+                    </h4>
+                    <p class="my-0">
+                      Class Size
+                    </p>
+                  </div>
+                </div>
+
+             </div>
+             <div class="img-box h-100 w-50 bg-secondary position-relative">
+               <img class="w-100 h-100 ms_fit_cover" src="../assets/images/class_01-690x506.jpg" alt="">
+               <button class="ms_btn_primary position-absolute bottom-0 end-0">
+                 Read More >
+               </button>
+             </div>
+           </div>
+
+         </div>
+         <div class="col-6">
+           <div class="ms_card debug-card d-flex">
+             <div class="text-box h-100 w-50 d-flex flex-column bg-primary justify-content-between">
+               <div class="text-box__top">
+               <h2>
+                 Little Lambs
+               </h2>
+               <p>Class Name</p>
+               </div>
+                <div class="text-box_bottom  bg-warning ">
+                  <div class="text-box_bottom_left my-1 w-50 d-inline-block text-center border-end">
+                    <h4 class="my-0">
+                      12-24
+                    </h4>
+                    <p class="my-0">
+                      Years old
+                    </p>
+                  </div>
+                  <div class="text-box_bottom_right my-1 w-50 d-inline-block text-center">
+                    <h4 class="my-0">
+                      9
+                    </h4>
+                    <p class="my-0">
+                      Class Size
+                    </p>
+                  </div>
+                </div>
+
+             </div>
+             <div class="img-box h-100 w-50 bg-secondary position-relative">
+               <img class="w-100 h-100 ms_fit_cover" src="../assets/images/class_01-690x506.jpg" alt="">
+               <button class="ms_btn_primary position-absolute bottom-0 end-0">
+                 Read More >
+               </button>
+             </div>
+           </div>
+
+         </div>
+      </div>
+  </section>
 </template>
 
 <script>
@@ -14,6 +188,13 @@ export default {
 <style lang="scss" scoped>
  @import '../assets/style/variables.scss';
 
- 
+
+
+
+//debug//
+.debug-card{
+  height: 200px;
+  border: 1px solid black;
+}
  
 </style>
