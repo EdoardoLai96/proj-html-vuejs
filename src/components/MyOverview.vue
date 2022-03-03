36ce5ec7a900263e17a863025468c8bc64a066dd
Edoardo Lai <edoardolai96@gmail.com>
2022-03-03 16:28:47 +0100

    layout overview

--- src/components/MyOverview.vue
+++ src/components/MyOverview.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>
-      
-  </div>
+  <section id="overview" class="debug-container">
+      <div class="container px-5 py-5">
+        <div class="row debug-row">
+          <div class="col debug-col d-flex flex-column">
+
+            <div class="btn-box">
+            <button class="ms_btn_primary ms_w-min">
+              overview
+            </button>
+            <button class="ms_btn_primary ms_w-min text-nowrap mx-1">
+              Our Mission
+            </button>
+            </div>
+
+            <p>
+              Our philosophy is learning throu play as we offer a stimulating environment for children.
+            </p>
+
+
+            <div class="badge-box-ver">
+              <div class="badge-icon-primary">
+                <img src="../assets/images/clock.png" alt="">
+              </div>
+              <div class="badge-text">
+                <h4 class="badge-title">Badge Title</h4>
+                <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+              </div>
+            </div>
+
+            <div class="badge-box-ver">
+              <div class="badge-icon-primary">
+                <img src="../assets/images/category.png" alt="">
+              </div>
+              <div class="badge-text">
+                <h4 class="badge-title">Badge Title</h4>
+                <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+              </div>
+            </div>
+
+
+          </div>
+          <div class="col debug-col">
+              SLIDER DA INSERIRE
+          </div>
+        </div>
+      </div>
+  </section>
 </template>
 
 <script>
--- src/components/MyWelcome.vue
+++ src/components/MyWelcome.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="welcome" class="debug-container">
-      <div class="container py-4">
+      <div class="container py-4 px-5">
         <div class="row">
           <div class="col debug-col text-center">
             <h2>Welcome to Fable</h2>
@@ -10,43 +10,52 @@
           </div>
         </div>
         <div class="row">
+
           <div class="col debug-col d-flex justify-content-center">
-            <div class="badge-box">
-            <div class="badge-icon-primary">
-              <img src="../assets/images/stroller_alt.png" alt="">
-            </div>
-              <h4 class="badge-title">Badge Title</h4>
-              <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+            <div class="badge-box-hor">
+              <div class="badge-icon-primary">
+                <img src="../assets/images/stroller_alt.png" alt="">
+              </div>
+              <div class="badge-text">
+                <h4 class="badge-title">Badge Title</h4>
+                <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+              </div>
             </div>
           </div>
 
           <div class="col debug-col d-flex justify-content-center">
-            <div class="badge-box">
-            <div class="badge-icon-secondary">
-              <img src="../assets/images/stroller_alt.png" alt="">
-            </div>
-              <h4 class="badge-title">Badge Title</h4>
-              <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
-            </div>
-          </div>
-
-          <div class="col debug-col d-flex justify-content-center">
-            <div class="badge-box">
-            <div class="badge-icon-primary">
-              <img src="../assets/images/stroller_alt.png" alt="">
-            </div>
-              <h4 class="badge-title">Badge Title</h4>
-              <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+            <div class="badge-box-hor">
+              <div class="badge-icon-secondary">
+                <img src="../assets/images/stroller_alt.png" alt="">
+              </div>
+              <div class="badge-text">
+                <h4 class="badge-title">Badge Title</h4>
+                <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+              </div>
             </div>
           </div>
 
           <div class="col debug-col d-flex justify-content-center">
-            <div class="badge-box">
-            <div class="badge-icon-secondary">
-              <img src="../assets/images/stroller_alt.png" alt="">
+            <div class="badge-box-hor">
+              <div class="badge-icon-primary">
+                <img src="../assets/images/stroller_alt.png" alt="">
+              </div>
+              <div class="badge-text">
+                <h4 class="badge-title">Badge Title</h4>
+                <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+              </div>
             </div>
-              <h4 class="badge-title">Badge Title</h4>
-              <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+          </div>
+
+          <div class="col debug-col d-flex justify-content-center">
+            <div class="badge-box-hor">
+              <div class="badge-icon-secondary">
+                <img src="../assets/images/stroller_alt.png" alt="">
+              </div>
+              <div class="badge-text">
+                <h4 class="badge-title">Badge Title</h4>
+                <p class="badge-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ratione! Perferendis inventore nam aliquid minima pariatur </p>
+              </div>
             </div>
           </div>
 
