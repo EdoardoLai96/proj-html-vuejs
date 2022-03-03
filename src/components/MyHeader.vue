9dae0a3aa0fe66aab8c344af854b2b8318cef898
Edoardo Lai <edoardolai96@gmail.com>
2022-03-03 13:13:26 +0100

    Stilizzazione header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>
-      
-  </div>
+  <header class="container-fluid debug-container px-5 align-items-center">
+      <div class="row debug-row justify-content-between"> 
+        <div class="col-5 debug-col">
+
+        </div>
+
+
+        <div class="col-1 debug-col">
+          <img src="" alt="">
+          <p class="text-center">Text</p>
+        </div>
+        <div class="col-1 debug-col">
+          <img src="" alt="">
+          <p class="text-center">Text</p>
+        </div>
+        <div class="col-1 debug-col">
+          <img src="" alt="">
+          <p class="text-center">Text</p>
+        </div>
+        <div class="col-1 debug-col">
+          <img src="" alt="">
+          <p class="text-center">Text</p>
+        </div>
+        <div class="col-1 debug-col">
+          <img src="" alt="">
+          <p class="text-center">Text</p>
+        </div>
+        <div class="col-1 debug-col">
+          <img src="" alt="">
+          <p class="text-center">Text</p>
+        </div>
+      </div>
+  </header>
 </template>
 
 <script>
@@ -15,5 +45,26 @@ export default {
  @import "../assets/style/variables.scss";
 
 
+
+
+
+
+
+//Debug// 
+
+.debug-col{
+  background-color: #1bafe4;
+  height: 70px;
+  margin: 1px;
+  
+  }
+.debug-row{
+  background-color: #fcd902;
+  
+}
+.debug-container{
+  background-color: #d71938;
+
+}
  
 </style>
