0812e4bca0aa92da42c81c5a3fad8f1552f1c61c
Edoardo Lai <edoardolai96@gmail.com>
2022-03-04 11:06:59 +0100

    Layout What's new e creazione cards

--- src/components/MyWhatsNew.vue
+++ src/components/MyWhatsNew.vue
@@ -1,12 +1,45 @@
 <template>
-  <section id="whatsnew" class="d-flex align-items-center">
+  <section id="whatsnew" class="d-flex align-items-center debug-container">
+    <div class="container">
+          <div class="row debug-row">
+        <div class="col debug-col text-center">
+        <h2>Welcome to Fable</h2>
+            <p>Family daycare, preschool, and kindergarten</p>
+            <p>/\/\/\/</p>
+
+        </div>
+      </div>
+      <div class="row debug row">
+        <div class="col-4 p-3">
+          <MyCardVert/>
+        </div>
+        <div class="col-4 p-3">
+          <MyCardVert/>
+        </div>
+        <div class="col-4 p-3">
+          <MyCardVert/>
+        </div>
+        <div class="col-12 text-center">
+          <button class="ms_btn_primary my-3">
+            GO TO BLOG >
+          </button>
+        </div>
+      </div>
+      
+    </div>
+
  
   </section>
 </template>
 
 <script>
+import MyCardVert from './partials/MyCardVert.vue'
+
 export default {
-    name: "MyWhatsNew"
+    name: "MyWhatsNew",
+    components:{
+      MyCardVert
+    }
 
 }
 </script>
---
+++ src/components/partials/MyCardVert.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="MyCardVert">
+      <span>Date</span>
+      <div class="img-box w-100 h-100 position-relative">
+          <img class="w-100" src="../../assets/images/blog_09-300x180.jpg" alt="">
+          <div class="partecipants position-absolute bottom-0 end-0">
+              42
+          </div>
+      </div>
+        <h4>Card Title</h4>
+        <p>Card Caption</p>
+        <hr class="my-1">
+        <div class="card_footer"> 
+            <img class="me-3 " src="../../assets/images/author.png" alt="">
+            <span class="align-middle">Author</span>
+            <img class="me-3 ms-3" src="../../assets/images/category.png" alt="">
+            <span class="align-middle">Category</span>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+name: "MyCardVert"
+}
+</script>
+
+<style lang="scss" scoped>
+ @import '../../assets/style/variables.scss';
+
+.partecipants{
+    background-color: $primaryColor;
+    padding: 0.2rem 0.4rem ;
+    color: white;
+}
+
+
+
+</style>
