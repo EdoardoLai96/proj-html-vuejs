9a83124f0a9c2ab2feb314a407a927dc475456e7
Edoardo Lai <edoardolai96@gmail.com>
2022-03-07 17:03:03 +0100

    creati custom balloons in overview

--- src/components/MyOverview.vue
+++ src/components/MyOverview.vue
@@ -5,12 +5,12 @@
           <div class="col-5 d-flex flex-column">
 
             <div class="btn-box">
-            <button class="ms_btn_primary ms_w-min">
-              overview
-            </button>
-            <button class="ms_btn_primary ms_w-min text-nowrap mx-1">
+            <div class="ms_w-min ms_balloon_active bottom bg-prim">
+              Overview
+            </div>
+            <div class="ms_balloon_non_active text-secondary d-inline-block ms-2">
               Our Mission
-            </button>
+            </div>
             </div>
 
             <p class="fs-3 mt-4 lh-sm seco">
@@ -78,7 +78,7 @@ export default {
           },
         ],
 
-        active : 0
+        active : 2
       }
     },
     methods: {
@@ -123,5 +123,44 @@ object-fit: contain;
     border-bottom: 2px solid red;
     padding-bottom: 1rem;
 }
+
+.ms_balloon_active{
+    display: inline-block;
+    height: auto;
+    position: relative;
+    padding: 0.45rem 1rem;
+    font-size: 0.8rem;
+    color: white;
+  &:after {
+    content: '';
+    position: absolute;
+    border: 1px solid rgba(51, 51, 51, 0.19);
+  }
+}
+
+  .bottom:after {
+    border-color: #fe6601 transparent;
+    border-width: 10px 6px 0 6px;
+    bottom: -7px;
+    left: 46%;
+      }
+  .bottom:before {
+    border-color: #999 transparent;
+    border-width: 11px 13px 0 13px;
+    bottom: -13px;
+    left: 45%;
+  }
+
+ .ms_balloon_non_active{
+   border: 1px solid rgba(223, 223, 223, 0.7);
+    display: inline-block;
+    height: auto;
+    position: relative;
+    padding: 0.45rem 1rem;
+    font-size: 0.8rem;
+    box-shadow:  0px 1px 0px  #d5d5d5;
+
+ }
+
 
 </style>
